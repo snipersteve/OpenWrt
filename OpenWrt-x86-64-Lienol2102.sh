40d2148913280b82e832a4aef2b0cc5b941e82e0
jinlife <glucose1e@tom.com>
2021-05-03 09:51:02 +0800

    2102 not compatible luci

--- OpenWrt-x86-64-Lienol2102.sh
+++ OpenWrt-x86-64-Lienol2102.sh
@@ -4,9 +4,17 @@
 # 安装额外依赖软件包
 # sudo -E apt-get -y install rename
 
+# 更新feeds文件
+sed -i 's#src-git luci https://github.com/Lienol/openwrt-luci.git;21.02#src-git luci https://github.com/Lienol/openwrt-luci.git;19.07#g' feeds.conf.default #更换luci版本
+cat feeds.conf.default
+
+#echo -e "\nsrc-git xiangfeidexiaohuo https://github.com/xiangfeidexiaohuo/openwrt-packages" >> feeds.conf.default
+# echo -e "src-git passwall https://github.com/xiaorouji/openwrt-passwall" >> feeds.conf.default
+
+
 # 添加第三方软件包
-# git clone https://github.com/db-one/dbone-packages.git -b 19.07 package/dbone-packages
-git clone https://github.com/garypang13/openwrt-packages package/graypang13-packages
+git clone https://github.com/db-one/dbone-packages.git -b 19.07 package/dbone-packages
+#git clone https://github.com/garypang13/openwrt-packages package/graypang13-packages
 
 # 更新并安装源
 ./scripts/feeds clean
